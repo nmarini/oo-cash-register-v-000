ff2e75c0a8e4da34e5cbff50a7d781211f6572e3
Nicolas Marini <nmarini44@gmail.com>
2018-06-19 23:59:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :last_transaction
 
 
   def initialize(discount = 0)
@@ -13,8 +13,10 @@ class CashRegister
    quantity.times do
      items << title
    end
+   self.last_transaction = amount * quantity  
 
  end
+ 
  def apply_discount
      if discount != 0
        self.total = (total * ((100.0 - discount.to_f)/100)).to_i
